fc4d9faea75813cc9519c5637c8c4fd334561def
vrey2 <91304765+vrey2@users.noreply.github.com>
2023-12-07 22:28:12 -0800

    Final composition

---
+++ CSE-111-Project-1/CSE-111-Project-1/importdata.py
@@ -0,0 +1,38 @@
+#Jie Jenn on Youtube 'Upload A CSV File' youtube.com/watch?v=UZIhVmkrAEs
+
+import sqlite3
+import pandas as pd
+
+connection = sqlite3.connect('data.db')
+
+dr = pd.read_csv('drivers.csv')
+
+dr.columns = dr.columns.str.strip() #gets rid of spaces
+
+dr.to_sql('drivers', connection, if_exists ='replace')
+
+ds = pd.read_csv('bus.csv')
+
+ds.columns = ds.columns.str.strip()
+
+ds.to_sql('busses', connection, if_exists='replace')
+
+dt = pd.read_csv('routes.csv')
+
+dt.columns = dt.columns.str.strip() #gets rid of spaces
+
+dt.to_sql('routes', connection, if_exists='replace')
+
+du = pd.read_csv('schedule.csv')
+
+du.columns = du.columns.str.strip()
+
+du.to_sql('schedule', connection, if_exists='replace')
+
+dv = pd.read_csv('busstops.csv')
+
+dv.columns = dv.columns.str.strip()
+
+dv.to_sql('busstops', connection, if_exists='replace')
+
+connection.close()
